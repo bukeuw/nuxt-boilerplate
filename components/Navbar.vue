c45ff696bc10d561b68270091ec7392752c5eccb
Santo Blasius <sntblasius@gmail.com>
2021-03-08 15:34:58 +0700

    Add navbar for the blog

---
+++ components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-navbar toggleable="lg" variant="primary">
+    <b-navbar-brand href="#">
+      Blog
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="navbar-collapse" />
+
+    <b-collapse id="navbar-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="/">
+          Home
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item v-if="!$auth.loggedIn" href="/login" right>
+          Login
+        </b-nav-item>
+
+        <b-nav-item-dropdown v-else right>
+          <template slot="button-content">
+            {{ $auth.user.name }}
+          </template>
+
+          <b-dropdown-item href="#">
+            My Post
+          </b-dropdown-item>
+
+          <b-dropdown-item href="#">
+            Logout
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
