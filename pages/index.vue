6a8fc7ba676e5c1d5b00f4a0ec116fcef518db47
Santo Blasius <sntblasius@gmail.com>
2021-03-08 15:35:27 +0700

    Move navbar to dedicated component

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,9 @@
 <template>
-  <el-container>
-    <el-header>
-      <el-menu mode="horizontal" router>
-        <el-menu-item index="/">
-          Home
-        </el-menu-item>
-        <el-submenu index="2">
-          <template v-if="$auth.loggedIn">
-            <template slot="title">
-              {{ $auth.user.name }}
-            </template>
-            <el-menu-item index="/profile">
-              Profile
-            </el-menu-item>
-            <el-menu-item index="2-1" @click="logout">
-              Logout
-            </el-menu-item>
-          </template>
-          <template v-else>
-            <template slot="title">
-              Account
-            </template>
-            <el-menu-item index="/login">
-              Login
-            </el-menu-item>
-            <el-menu-item index="/register">
-              Register
-            </el-menu-item>
-          </template>
-        </el-submenu>
-      </el-menu>
-    </el-header>
-    <el-main>
-      <el-card title="home">
-        <span>home</span>
-      </el-card>
-    </el-main>
-  </el-container>
+  <div class="container">
+    <div class="p">
+      home
+    </div>
+  </div>
 </template>
 
 <script>
